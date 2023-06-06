973e9d3a3a7e0ec7daab647ec638d7f3f8039aea
Adriana Rodriguez Gaona <Adri_mrg@yahoo.com>
2023-06-06 13:12:22 +0200

    working with an API instead of mock data

--- BethanysPieShopHRM.App/Components/EmployeeCard.razor.cs
+++ BethanysPieShopHRM.App/Components/EmployeeCard.razor.cs
@@ -7,5 +7,24 @@ namespace BethanysPieShopHRM.App.Components
     {
         [Parameter]
         public Employee Employee { get; set; } = default!;
+
+        [Parameter]
+        public EventCallback<Employee> EmployeeQuickViewClicked { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = default!;
+
+        protected override void OnInitialized()
+        {
+	        if (string.IsNullOrEmpty(Employee.LastName))
+	        {
+		        throw new Exception("last name can't be empty");
+	        }
+        }
+
+        public void NavigateToDetails(Employee selectedEmployee)
+        {
+            NavigationManager.NavigateTo($"/employeedetail/{selectedEmployee.EmployeeId}");
+        }
     }
 }
--- BethanysPieShopHRM.App/Pages/EmployeeOverview.razor.cs
+++ BethanysPieShopHRM.App/Pages/EmployeeOverview.razor.cs
@@ -6,9 +6,17 @@ namespace BethanysPieShopHRM.App.Pages
     public partial class EmployeeOverview
     {
         public List<Employee>? Employees { get; set; } = default!;
+
+        private Employee? _selectedEmployee;
+        private string Title = "Employee overview";
         protected override void OnInitialized()
         {
             Employees = MockDataService.Employees;
+        }
+
+        public void ShowQuickViewPopup(Employee selectedEmployee)
+        {
+            _selectedEmployee = selectedEmployee;
         }
     }
 }
