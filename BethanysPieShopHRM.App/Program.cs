11fc3fb50432725c39a996d681d32abde438c011
Adriana Rodriguez Gaona <Adri_mrg@yahoo.com>
2023-06-07 08:44:30 +0200

    Edit of add employee components add

--- BethanysPieShopHRM.App/Program.cs
+++ BethanysPieShopHRM.App/Program.cs
@@ -1,4 +1,6 @@
 using BethanysPieShopHRM.App;
+using BethanysPieShopHRM.App.Services;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +8,15 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(client =>
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+builder.Services.AddHttpClient<ICountryDataService, CountryDataService>(client =>
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+builder.Services.AddHttpClient<IJobCategoryDataService, JobCategoryDataService>(client =>
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
+
+builder.Services.AddScoped<ApplicationState>();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
